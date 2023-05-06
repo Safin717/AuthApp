9dbb60f40fb83db1fa00dcbd58f5cb5f02130d4d
Azat <saa20@inbox.ru>
2023-05-06 14:57:53 +0300

    Add Firebase Auth, edited registration and authorization screens

--- app/src/main/java/com/example/authapp/LoginActivity.kt
+++ app/src/main/java/com/example/authapp/LoginActivity.kt
@@ -1,20 +1,67 @@
 package com.example.authapp
 
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.authapp.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
         binding.btnLogin.setOnClickListener {
-            startActivity(Intent(this,HomeActivity::class.java ))
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+
+            val emailPattern = Pattern.compile("^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$")
+
+            if(email.isNotEmpty() && password.isNotEmpty()){
+
+                if (!emailPattern.matcher(email).matches()) {
+                    binding.tilEmail.error = "Введите адрес электронной почты"
+                    binding.tilEmail.setErrorTextColor(ColorStateList.valueOf(Color.RED))
+                } else {
+                    binding.tilEmail.error = null
+                }
+
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+                        if(it.isSuccessful){
+                            startActivity(Intent(this, HomeActivity::class.java))
+                        } else {
+                            Toast.makeText(this, it.exception?.message, Toast.LENGTH_LONG).show()
+                        }
+                    }
+            } else {
+                Toast.makeText(this, "Заполните все поля", Toast.LENGTH_LONG).show()
+            }
+            binding.tvForgotPassword.setOnClickListener {
+                if(email.isNotEmpty()){
+                    firebaseAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(this, "Вам отправлено письмо на адрес электронной почты", Toast.LENGTH_LONG).show()
+                            } else {
+                                Toast.makeText(this, task.exception?.message, Toast.LENGTH_LONG).show()
+                            }
+                        }
+                } else {
+                    binding.tilEmail.error = "Введите адрес электронной почты"
+                    binding.tilEmail.setErrorTextColor(ColorStateList.valueOf(Color.RED))
+                }
+            }
         }
 
         binding.tvHaventAccount.setOnClickListener {
--- app/src/main/java/com/example/authapp/RegisterActivity.kt
+++ app/src/main/java/com/example/authapp/RegisterActivity.kt
@@ -1,20 +1,57 @@
 package com.example.authapp
 
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.authapp.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.delay
+import java.lang.Thread.sleep
+import java.util.regex.Pattern
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        firebaseAuth = FirebaseAuth.getInstance()
 
         binding.btnRegister.setOnClickListener {
-            startActivity(Intent(this, LoginActivity::class.java))
+
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+            val confirmPass = binding.etConfirmPassword.text.toString()
+            val emailPattern = Pattern.compile("^([a-zA-Z0-9_\\-\\.]+)@([a-zA-Z0-9_\\-\\.]+)\\.([a-zA-Z]{2,5})$")
+
+            if(email.isNotEmpty() && password.isNotEmpty() && confirmPass.isNotEmpty()){
+                if (!emailPattern.matcher(email).matches()) {
+                    binding.tilEmail.error = "Введите корректный адрес электронной почты"
+                    binding.tilEmail.setErrorTextColor(ColorStateList.valueOf(Color.RED))
+                } else {
+                    binding.tilEmail.error = null
+                }
+                if(password == confirmPass){
+                    firebaseAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener {
+                            if(it.isSuccessful){
+                                Toast.makeText(this, "Вы зарегистрированы", Toast.LENGTH_LONG).show()
+                                startActivity(Intent(this, LoginActivity::class.java))
+                            } else {
+                                Toast.makeText(this, it.exception?.message, Toast.LENGTH_LONG).show()
+                            }
+                        }
+                }else{
+                    Toast.makeText(this, "Пароли не совпадают", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                Toast.makeText(this, "Заполните все поля", Toast.LENGTH_LONG).show()
+            }
         }
         binding.tvHaveAccount.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
